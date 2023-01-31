fc2870aec75435fa598e49b68da514e83f7df4ed
mohammed <togachivon@gmail.com>
2023-01-31 06:18:28 +0100

    light

---
+++ Assets/scripts/light.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+    public GameObject _light;
+    
+
+    bool playerIsHere;
+    public float secsToNext = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerIsHere = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsHere && Input.GetKeyUp("l") )
+        {
+            _light.SetActive(!_light.activeSelf);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "PlayerArmature")
+            playerIsHere = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "PlayerArmature")
+            playerIsHere = false;
+    }
+}
